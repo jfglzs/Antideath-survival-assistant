63ed2e02b56077d6249253c5f2b5ee7f84fdbffb
antideath <243911689@qq.com>
2025-09-06 16:53:27 +0800

    Update : readme

--- src/main/java/io/github/jfglzs/feature/asaSwitchServer.java
+++ src/main/java/io/github/jfglzs/feature/asaSwitchServer.java
@@ -1,10 +1,8 @@
 package io.github.jfglzs.feature;
 
-import io.github.jfglzs.config.Configs;
-
 import java.util.List;
 
-import static io.github.jfglzs.AsaMod.space;
+import static io.github.jfglzs.AsaMod.SPACE;
 import static io.github.jfglzs.config.Configs.SWITCH_SERVER_COMMAND;
 import static io.github.jfglzs.config.Configs.SWITCH_SERVER_LIST;
 import static io.github.jfglzs.utils.CommandUtils.runCommand;
@@ -17,7 +15,7 @@ public class asaSwitchServer {
         List<String> list = SWITCH_SERVER_LIST.getStrings();
         String[] servers = list.toArray(new String[0]);
 
-        runCommand(SWITCH_SERVER_COMMAND.getStringValue() + space + servers[index++]);
+        runCommand(SWITCH_SERVER_COMMAND.getStringValue() + SPACE + servers[index++]);
         System.out.println(index);
         if(index == servers.length) index = 0;
     }
