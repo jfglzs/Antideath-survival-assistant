0b8dc353766dc2d3dd38c8a8b7b9ba1810163efa
antideath <mineahd@outlook.com>
2026-02-13 00:25:07 +0800

    feat: MiniHud opti

--- src/main/java/io/jfglzs/asa/mixin/minihud/RenderHandler_Mixin.java
+++ src/main/java/io/jfglzs/asa/mixin/minihud/RenderHandler_Mixin.java
@@ -1,18 +1,14 @@
 package io.jfglzs.asa.mixin.minihud;
 
-import fi.dy.masa.malilib.config.HudAlignment;
-import fi.dy.masa.malilib.render.RenderUtils;
 import fi.dy.masa.minihud.event.RenderHandler;
 import io.jfglzs.asa.config.Configs;
 import io.jfglzs.asa.utils.ThreadUtils;
-import net.minecraft.client.gui.DrawContext;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -21,14 +17,19 @@ public abstract class RenderHandler_Mixin {
     @Shadow @Final @Mutable
     private List<String> lines;
     @Unique
-    private List<String> cache = new ArrayList<>();
+    private boolean isRunning = false;
     @Unique
-    private volatile boolean isRunning = false;
-    @Unique
-    private volatile boolean enabled = false;
-
+    private boolean enabled = false;
     @Shadow
     protected abstract void updateLines();
+
+    @Inject(
+            method = "<init>",
+            at = @At("TAIL")
+    )
+    public void init_Inject(CallbackInfo ci) {
+        this.lines = new CopyOnWriteArrayList<>();
+    }
 
     //#if MC >= 12105
     @Redirect(
@@ -52,8 +53,7 @@ public abstract class RenderHandler_Mixin {
                         this.updateLines();
                         Thread.sleep(50); // 每tick刷新一次，节约CPU资源
                     } catch (Exception e) {
-                        isRunning = false;
-                        Thread.currentThread().interrupt();
+                        throw new RuntimeException(e);
                     }
                 }
                 isRunning = false;
@@ -62,51 +62,4 @@ public abstract class RenderHandler_Mixin {
             this.updateLines();
         }
     }
-
-    @Inject(
-            method = "updateLines",
-            at = @At("TAIL")
-    )
-    private void minihudOpt_inject(CallbackInfo ci) {
-        if (enabled) return;
-        List<String> l = List.copyOf(lines);
-        synchronized (this) {
-            this.cache = l;
-        }
-    }
-
-
-    @Redirect(
-            //#if MC >= 12105
-            method = "onRenderGameOverlayPostAdvanced",
-            //#else-if MC < 12105
-            //$$ method = "onRenderGameOverlayPost",
-            //#endif
-            //#if MC == 12105
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lfi/dy/masa/malilib/render/RenderUtils;renderText(IIDIILfi/dy/masa/malilib/config/HudAlignment;ZZZLjava/util/List;Lnet/minecraft/client/gui/DrawContext;)I"
-            )
-            //#endif
-            //#if MC <= 12104
-            //$$ at = @At(value = "INVOKE", target = "Lfi/dy/masa/malilib/render/RenderUtils;renderText(IIDIILfi/dy/masa/malilib/config/HudAlignment;ZZZLjava/util/List;Lnet/minecraft/client/gui/DrawContext;)I")
-            //#endif
-    )
-    public int minihudOpt_redirect(int x, int y, double f, int c, int bx, HudAlignment a, boolean b, boolean b2, boolean b3, List<String> l2, DrawContext cn) {
-        List<String> l;
-        synchronized (this) {
-            // 如果异步线程还没跑完第一次，先用原 list 兜底
-            l = this.cache.isEmpty() || !enabled ? l2 : this.cache;
-        }
-        return RenderUtils.renderText(x, y, f, c, bx, a, b, b2, b3, l, cn);
-    }
-
-    @Inject(
-            method = "<init>",
-            at = @At("TAIL")
-    )
-    public void init_Inject(CallbackInfo ci) {
-        this.lines = new CopyOnWriteArrayList<>();
-    }
-
 }
