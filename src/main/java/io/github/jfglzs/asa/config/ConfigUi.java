a68070966d52955508196dcc6b129a48f4a6eb92
antideath <mineahd@outlook.com>
2026-02-13 19:21:40 +0800

    fix : some bugs

---
+++ src/main/java/io/github/jfglzs/asa/config/ConfigUi.java
@@ -0,0 +1,89 @@
+package io.github.jfglzs.asa.config;
+
+import fi.dy.masa.malilib.config.IConfigBase;
+import fi.dy.masa.malilib.gui.GuiConfigsBase;
+import fi.dy.masa.malilib.gui.button.ButtonBase;
+import fi.dy.masa.malilib.gui.button.ButtonGeneric;
+import fi.dy.masa.malilib.gui.button.IButtonActionListener;
+import fi.dy.masa.malilib.util.StringUtils;
+
+import java.util.List;
+
+import static io.github.jfglzs.asa.AsaMod.*;
+
+public class ConfigUi extends GuiConfigsBase {
+    private static Tab tab = Tab.ALL;
+
+
+    public ConfigUi()
+    {
+        super(10, 50, MOD_ID, null, "配置界面");
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+        this.clearOptions();
+
+        int x = 10;
+        int y = 26;
+        for (Tab tab : Tab.values()) {
+            x += this.createButton(x, y, -1, tab);
+        }
+    }
+
+    private int createButton(int x, int y, int width, Tab tab) {
+        ButtonGeneric button = new ButtonGeneric(x, y, width, 20, tab.getTranslate());
+        button.setEnabled(ConfigUi.tab != tab);
+        this.addButton(button, new ButtonListener(tab, this));
+
+        return button.getWidth() + 2;
+    }
+
+    @Override
+    public List<ConfigOptionWrapper> getConfigs() {
+        List<? extends IConfigBase> configs;
+        Tab tab = ConfigUi.tab;
+        if (tab == Tab.ALL) {
+            configs = Configs.ALL_CONFIGS;
+        } else {
+            configs = Configs.ALL_CONFIGS;
+        }
+        return ConfigOptionWrapper.createFor(configs);
+    }
+
+    private static class ButtonListener implements IButtonActionListener {
+        private final ConfigUi parent;
+        private final Tab tab;
+
+        public ButtonListener(Tab tab, ConfigUi parent) {
+            this.tab = tab;
+            this.parent = parent;
+        }
+
+        @Override
+        public void actionPerformedWithButton(ButtonBase button, int mouseButton) {
+            ConfigUi.tab = this.tab;
+            this.parent.reCreateListWidget();
+            this.parent.getListWidget().resetScrollbarPosition();
+            this.parent.initGui();
+        }
+    }
+
+    public enum Tab {
+        ALL("所有"),
+        ;
+
+        public final String translation;
+
+        Tab(String translation) {
+            this.translation = translation;
+        }
+
+        public String getTranslate()
+        {
+            return StringUtils.translate(translation);
+        }
+    }
+
+}
---
+++ src/main/java/io/github/jfglzs/asa/mixin/feature/SubtitlesHud_Mixin.java
@@ -0,0 +1,24 @@
+package io.github.jfglzs.asa.mixin.feature;
+
+import io.github.jfglzs.asa.config.Configs;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.SubtitlesHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SubtitlesHud.class)
+public class SubtitlesHud_Mixin
+{
+    @Inject(
+            method = "render",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void renderInject(DrawContext context, CallbackInfo ci) {
+        if (Configs.DISABLE_SUBTITLE.getBooleanValue() && Configs.shouldDisableTitle) {
+            ci.cancel();
+        }
+    }
+}
