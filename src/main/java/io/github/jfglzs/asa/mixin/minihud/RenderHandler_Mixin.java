3da08b7c6a6ef0c3de3d3c7407e44035b2279769
antideath <mineahd@outlook.com>
2026-03-08 22:22:08 +0800

    feat:

--- src/main/java/io/github/jfglzs/asa/mixin/minihud/RenderHandler_Mixin.java
+++ src/main/java/io/github/jfglzs/asa/mixin/minihud/RenderHandler_Mixin.java
@@ -2,7 +2,6 @@ package io.github.jfglzs.asa.mixin.minihud;
 
 import fi.dy.masa.minihud.event.RenderHandler;
 import io.github.jfglzs.asa.config.Configs;
-import io.github.jfglzs.asa.utils.ThreadUtils;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,16 +10,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Future;
 
 @Mixin(RenderHandler.class)
 public abstract class RenderHandler_Mixin {
     @Shadow
-    @Final
     @Mutable
+    @Final
     private List<String> lines;
-    @Unique
-    private Future future;
+
     @Unique
     private boolean enabled = false;
     @Shadow
@@ -36,31 +33,20 @@ public abstract class RenderHandler_Mixin {
 
     //#if MC >= 12105
     @Redirect(
+            //#if MC >= 12105
             method = "onRenderGameOverlayPostAdvanced",
+            //#else
+            //$$ method = "onRenderGameOverlayPost",
+            //#endif
             at = @At(value = "INVOKE", target = "Lfi/dy/masa/minihud/event/RenderHandler;updateLines()V")
     )
-    //#else
-    //$$    @Redirect(
-    //$$            method = "onRenderGameOverlayPost",
-    //$$            at = @At(value = "INVOKE", target = "Lfi/dy/masa/minihud/event/RenderHandler;updateLines()V")
-    //$$    )
-    //#endif
-    private void minihudOpt_Redirect(RenderHandler instance) {
+    private void updateLinesRedirect(RenderHandler instance) {
         enabled = Configs.MINIHUD_POTIMIZE.getBooleanValue();
-
-        if (enabled && future != null && !future.isDone() || enabled && future == null) {
-            future = ThreadUtils.threadPool.submit(() -> {
-                while (enabled) {
-                    try {
-                        this.updateLines();
-                        Thread.sleep(50); // 每tick刷新一次，节约CPU资源
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });
-        } else if (!enabled) {
+        if (enabled) {
+            Thread.startVirtualThread(this::updateLines);
+        } else {
             this.updateLines();
         }
     }
 }
+
