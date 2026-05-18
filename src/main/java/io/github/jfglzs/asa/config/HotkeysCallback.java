6357573492f03b7cc8be77bb1b010d9e13846a3f
antideath <mineahd@outlook.com>
2026-05-18 22:35:37 +0800

    feat:假人全物品

--- src/main/java/io/github/jfglzs/asa/config/HotkeysCallback.java
+++ src/main/java/io/github/jfglzs/asa/config/HotkeysCallback.java
@@ -5,12 +5,9 @@ import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
 import fi.dy.masa.malilib.hotkeys.IKeybind;
 import fi.dy.masa.malilib.hotkeys.KeyAction;
 import io.github.jfglzs.asa.AsaMod;
-import io.github.jfglzs.asa.feature.switchserver.AsaSwitchServer;
 import io.github.jfglzs.asa.render.MaterialToDoRenderer;
-import io.github.jfglzs.asa.utils.MCUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
-import net.minecraft.item.Item;
 
 import static io.github.jfglzs.asa.config.Configs.*;
 
@@ -21,22 +18,6 @@ public class HotkeysCallback implements IHotkeyCallback {
     public boolean onKeyAction(KeyAction action, IKeybind key) {
         if (key == ASA.getKeybind()) {
             client.setScreen(new ConfigUi());
-            return true;
-        }
-        else if (key == SWITCH_SERVER.getKeybind()) {
-            AsaSwitchServer.switchServer();
-            return true;
-        }
-        else if (key == SWITCH_SERVER_SINGLE_1.getKeybind()) {
-            AsaSwitchServer.switchServer(SWITCH_SERVER_SINGLE_1S.getStringValue());
-            return true;
-        }
-        else if (key == SWITCH_SERVER_SINGLE_2.getKeybind()) {
-            AsaSwitchServer.switchServer(SWITCH_SERVER_SINGLE_2S.getStringValue());
-            return true;
-        }
-        else if (key == SWITCH_SERVER_SINGLE_3.getKeybind()) {
-            AsaSwitchServer.switchServer(SWITCH_SERVER_SINGLE_3S.getStringValue());
             return true;
         }
         else if (key == CLEAR_MATERIAL_TODO_OVERLAY.getKeybind()) {
