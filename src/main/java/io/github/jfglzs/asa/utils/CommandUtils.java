e1f41ae5b288530b1f4a33dbd62901ef89a6a378
antideath <mineahd@outlook.com>
2026-05-22 22:01:15 +0800

    FIX:BUG修复

--- src/main/java/io/github/jfglzs/asa/utils/CommandUtils.java
+++ src/main/java/io/github/jfglzs/asa/utils/CommandUtils.java
@@ -1,15 +1,13 @@
 package io.github.jfglzs.asa.utils;
 
-import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientCommandSource;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 
 public class CommandUtils {
     public static boolean canUseCommand(String command) {
         ClientPlayNetworkHandler networkHandler = MinecraftClient.getInstance().getNetworkHandler();
         if (networkHandler != null) {
-            CommandDispatcher<ClientCommandSource> dispatcher = networkHandler.getCommandDispatcher();
+            var dispatcher = networkHandler.getCommandDispatcher();
             return dispatcher.getRoot().getChild(command) != null;
         }
         return false;
