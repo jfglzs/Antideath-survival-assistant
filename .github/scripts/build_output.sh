3000b11103a64459f3d4086db960f0a564d2733d
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2026-05-25 03:26:29 +0800

    更改构建脚本

--- .github/scripts/build_output.sh
+++
@@ -1,8 +0,0 @@
-dir=$(ls -l ./versions | awk '/^d/ {print $NF}')
-out_dir="./out"
-
-for i in $dir
-do
-    tmp_dir="./versions/$i/build/libs/*.jar"
-    cp $tmp_dir $out_dir
-done
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+#!/usr/bin/env kotlin
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter").version("0.9.2")
+}
+
+stonecutter {
+    create(rootProject) {
+        versions("1.21.1","1.21.5", "1.21.8").buildscript("build.gradle.kts")
+        vcsVersion = "1.21.5"
+    }
+}
+
+rootProject.name = "antideath-survival-assistant"
