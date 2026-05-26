ccf25d428a22b78e948985d97a917c89ac082c5b
antideath <mineahd@outlook.com>
2026-05-26 22:28:08 +0800

    UPDATE:自动打开假人背包

--- src/main/java/io/github/jfglzs/asa/mixin/feature/lms/AbstractContainerScreen_Mixin.java
+++ src/main/java/io/github/jfglzs/asa/mixin/feature/lms/AbstractContainerScreen_Mixin.java
@@ -1,14 +1,17 @@
 package io.github.jfglzs.asa.mixin.feature.lms;
 
 import io.github.jfglzs.asa.config.Configs;
+import io.github.jfglzs.asa.utils.MCUtils;
+import io.github.jfglzs.asa.utils.lms.ItemStorageDataManager;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
 
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreen_Mixin extends Screen {
@@ -17,12 +20,20 @@ public abstract class AbstractContainerScreen_Mixin extends Screen {
     }
 
     @Inject(
-            method = "keyPressed",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/AbstractContainerScreen;onClose()V")
+            method = "onClose",
+            at = @At("TAIL")
     )
-    public void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (((AbstractContainerScreen) (Object) this) instanceof CreativeModeInventoryScreen) {
-            Configs.lockCreativeScreen = false;
+    private void onClose(CallbackInfo ci) {
+        if (Configs.AUTO_KILL_FAKE_PLAYERS.getBooleanValue()) {
+            Set<String> fakePlayerNames = ItemStorageDataManager.getFakePlayerNames();
+            for (String name : fakePlayerNames) {
+                String titleString = this.title.getString();
+                if (titleString.contains(name)) {
+                    MCUtils.excuteCommand("player %s kill".formatted(name));
+                    fakePlayerNames.clear();
+                    break;
+                }
+            }
         }
     }
 }
