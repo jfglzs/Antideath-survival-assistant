6ec352e7270c866c4b254cd0859124b274382378
antideath <mineahd@outlook.com>
2026-08-17 15:24:33 +0800

    修复编译失败

--- src/main/java/io/github/jfglzs/asa/feature/disablePacketKick/ASAFakePacket.java
+++ src/main/java/io/github/jfglzs/asa/feature/disablePacketKick/ASAFakePacket.java
@@ -4,7 +4,6 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.PacketType;
 import net.minecraft.network.protocol.common.ClientCommonPacketListener;
 import net.minecraft.network.protocol.common.CommonPacketTypes;
-import org.jspecify.annotations.NonNull;
 
 public final class ASAFakePacket implements Packet<ClientCommonPacketListener> {
 
@@ -14,11 +13,11 @@ public final class ASAFakePacket implements Packet<ClientCommonPacketListener> {
     }
 
     @Override
-    public @NonNull PacketType<? extends Packet<ClientCommonPacketListener>> type() {
+    public PacketType<? extends Packet<ClientCommonPacketListener>> type() {
         return CommonPacketTypes.CLIENTBOUND_CUSTOM_PAYLOAD;
     }
 
     @Override
-    public void handle(@NonNull ClientCommonPacketListener listener) {
+    public void handle(ClientCommonPacketListener listener) {
     }
 }
