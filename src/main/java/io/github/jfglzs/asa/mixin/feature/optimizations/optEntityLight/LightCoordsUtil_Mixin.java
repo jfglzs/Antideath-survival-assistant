b2892fd6a497459e0f6ddccb6c3ce7b6794c7d32
antideath <mineahd@outlook.com>
2026-09-25 14:13:25 +0800

    修复编译失败

--- src/main/java/io/github/jfglzs/asa/mixin/feature/optimizations/optEntityLight/LightCoordsUtil_Mixin.java
+++ src/main/java/io/github/jfglzs/asa/mixin/feature/optimizations/optEntityLight/LightCoordsUtil_Mixin.java
@@ -1,8 +1,6 @@
 package io.github.jfglzs.asa.mixin.feature.optimizations.optEntityLight;
 
 import io.github.jfglzs.asa.config.Configs;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockAndLightGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,7 +16,7 @@ public class LightCoordsUtil_Mixin {
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void getLightCoords(BlockAndLightGetter level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+    private static void getLightCoords(CallbackInfoReturnable<Integer> cir) {
         if (Configs.Optimizations.OPT_ENTITY_LIGHT.getBooleanValue())
             cir.setReturnValue(15728880);
     }
